e61892c5f8b0d4f1f5f39a2a4d85d514a1820976
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-03-28 16:24:55 -0400

    Remove Active Storage.

--- app/controllers/noodles_controller.rb
+++ app/controllers/noodles_controller.rb
@@ -1,2 +1,19 @@
 class NoodlesController < ApplicationController
+
+  def create
+    byebug
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def index
+  end
+
 end
--- app/models/noodle.rb
+++ app/models/noodle.rb
@@ -1,4 +1,4 @@
 class Noodle < ApplicationRecord
   belongs_to :user
-  has_one_attached :recording
+  # has_one_attached :recording
 end
