53a740500e00dcd2d9f876bf871c472b0f3e8a26
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-03-29 22:56:27 -0400

    Add validations to models.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :vamps, dependent: :destroy
   has_secure_password
+  validates :username, uniqueness: true
+  validates :password, length: {in: 8...30}
 end
