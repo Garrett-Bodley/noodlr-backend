e1bf646d1cec23c90fbe8a6561031e255bcd8123
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-03-29 23:25:30 -0400

    Update auth methods to use .env secret.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,22 @@
 class ApplicationController < ActionController::API
+  before_action :authorized
 
   def encode_token(payload)
-    # don't forget to hide your secret in an environment variable
-    JWT.encode(payload, 'my_s3cr3t')
+    # should store secret in env variable
+    JWT.encode(payload, jwt_secret)
   end
 
   def auth_header
+    # { Authorization: 'Bearer <token>' }
     request.headers['Authorization']
   end
 
   def decoded_token
     if auth_header
       token = auth_header.split(' ')[1]
+      # header: { 'Authorization': 'Bearer <token>' }
       begin
-        JWT.decode(token, 'my_s3cr3t', true, algorithm: 'HS256')
+        JWT.decode(token, jwt_secret, true, algorithm: 'HS256')
       rescue JWT::DecodeError
         nil
       end
@@ -22,8 +25,6 @@ class ApplicationController < ActionController::API
 
   def current_user
     if decoded_token
-      # decoded_token=> [{"user_id"=>2}, {"alg"=>"HS256"}]
-      # or nil if we can't decode the token
       user_id = decoded_token[0]['user_id']
       @user = User.find_by(id: user_id)
     end
@@ -31,5 +32,15 @@ class ApplicationController < ActionController::API
 
   def logged_in?
     !!current_user
+  end
+
+  def authorized
+    render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
+  end
+
+  private
+
+  def jwt_secret
+    ENV['JWT_SECRET']
   end
 end 
