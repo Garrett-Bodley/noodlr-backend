541e03ae89cc8eb39a1085eb66f0bf9fe8348987
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-04-02 02:37:25 -0400

    Build out create action.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 class UsersController < ApplicationController
+  skip_before_action :authorized, only: [:create]
+
 
   def create
-    byebug
+    user = User.create(user_params)
+    if user.valid?
+      token = encode_token(user_id: user.id)
+      render json: {user: UserSerializer.new(user), token: token}, status: :created
+    else
+      render json: { message: 'Invalid username or password' }, status: :not_acceptable
+    end
   end
 
   def show
@@ -12,4 +20,11 @@ class UsersController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+  
 end
