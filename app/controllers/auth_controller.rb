cb1c10243b65da42c65106d60c417f3ac53ce630
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-04-02 02:36:59 -0400

    Move auth controller in file structure.

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,13 +1,13 @@
-class Api::V1::AuthController < ApplicationController
+class AuthController < ApplicationController
   skip_before_action :authorized, only: [:create]
 
   def create
-    @user = User.find_by(username: user_params[:username])
+    user = User.find_by(username: user_params[:username])
     #User#authenticate comes from BCrypt
-    if @user && @user.authenticate(user_params[:password])
+    if user && user.authenticate(user_params[:password])
       # encode token comes from ApplicationController
-      token = encode_token({ user_id: @user.id })
-      render json: { user: UserSerializer.new(@user), jwt: token }, status: :accepted
+      token = encode_token( user_id: user.id )
+      render json: {user: UserSerializer.new(user), token: token}, status: :accepted
     else
       render json: { message: 'Invalid username or password' }, status: :unauthorized
     end
