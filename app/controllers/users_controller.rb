9fe8994c5444b2d8c4c1264b716ede501a7732c1
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-04-09 14:48:30 -0400

    Build out #update action. Update user_params accordingly.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,10 +12,32 @@ class UsersController < ApplicationController
     end
   end
 
-  def show
+  def update
+    user = User.find_by(id: user_params[:id])
+    if user == current_user && user.authenticate(user_params[:password])
+      if !!user_params[:new_password] && !!user_params[:new_password_confirmation]
+        begin 
+          user.update(username: user_params[:username], password: user_params[:new_password], password_confirmation: user_params[:password_confirmation])
+        rescue
+          render json: { message: 'New password and password confirmation fields must match' }, status: :not_acceptable
+        else
+          render json: user, status: :ok
+        end
+      elsif 
+        begin
+          user.update(username: user_params[:username])
+        rescue
+          render json: { message: 'Invalid username supplied'}
+        else
+          render json: user, status: :ok
+        end
+      end
+    else
+      render json: { message: 'Invalid username or password' }, status: :not_acceptable
+    end
   end
 
-  def edit
+  def show
   end
 
   def destroy
@@ -24,7 +46,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password)
+    params.require(:user).permit(:id, :username, :password, :password_confirmation, :new_password, :new_password_confirmation)
   end
   
 end
