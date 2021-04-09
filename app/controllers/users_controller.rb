155eeeac5495190fa04b278990fbf2b50c85bb05
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-04-09 19:24:09 -0400

    Handle exceptions for update action. Build out destroy action.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,39 +8,37 @@ class UsersController < ApplicationController
       token = encode_token(user_id: user.id)
       render json: {user: UserSerializer.new(user), token: token}, status: :created
     else
-      render json: { message: 'Invalid username or password' }, status: :not_acceptable
+      render json: { messages: ['Invalid username or password'] }, status: :not_acceptable
     end
   end
 
   def update
     user = User.find_by(id: user_params[:id])
     if user == current_user && user.authenticate(user_params[:password])
-      if !!user_params[:new_password] && !!user_params[:new_password_confirmation]
-        begin 
-          user.update(username: user_params[:username], password: user_params[:new_password], password_confirmation: user_params[:password_confirmation])
-        rescue
-          render json: { message: 'New password and password confirmation fields must match' }, status: :not_acceptable
-        else
+      if !user_params[:new_password].empty? && !user_params[:new_password_confirmation].empty?
+        user.update(username: user_params[:username], password: user_params[:new_password], password_confirmation: user_params[:new_password_confirmation])
+        if user.valid?
           render json: user, status: :ok
+        else
+          render json: { messages: user.errors.full_messages }, status: :not_acceptable
         end
-      elsif 
-        begin
-          user.update(username: user_params[:username])
-        rescue
-          render json: { message: 'Invalid username supplied'}
-        else
+      else 
+        user.update(username: user_params[:username])
+        if user.valid?
           render json: user, status: :ok
+        else
+          render json: { messages: user.errors.full_messages}, status: :not_acceptable
         end
       end
     else
-      render json: { message: 'Invalid username or password' }, status: :not_acceptable
+      render json: { messages: ['Invalid username or password'] }, status: :not_acceptable
     end
   end
 
-  def show
-  end
-
   def destroy
+    user = current_user
+    user.destroy
+    render json: {messages: ['Account deleted']}, status: :ok
   end
 
   private
