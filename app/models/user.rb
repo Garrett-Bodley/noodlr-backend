b472801d951b64dd6b025e2163a364247da47941
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-04-09 19:24:37 -0400

    Only validate password if present.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_many :vamps, dependent: :destroy
   has_secure_password
   validates :username, uniqueness: true
-  validates :password, length: {in: 6...30}
+  validates :password, length: {in: 6...30}, if: :password
 end
